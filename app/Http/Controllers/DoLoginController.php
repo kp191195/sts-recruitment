3c310a592b5a13040b10880f90d5cc1f9cd4a785
kp <kp191195@gmail.com>
2017-09-21 08:33:25 +0000

    memperbaiki model MUser

--- app/Http/Controllers/DoLoginController.php
+++ app/Http/Controllers/DoLoginController.php
@@ -8,10 +8,18 @@ use App\Http\Requests;
 
 use Log;
 
+use App\MUser;
+
 class DoLoginController extends Controller
 {
     //
     public function getLogins(Request $request){
-        Log::debug($request->all());
+        //Log::debug($request->all());
+        
+        $input = $request->all();
+        $loggedUser = MUser::whereRaw('email = ? AND password = ?',[$input['uname'],md5($input['psw'])])->first();
+
+        Log::debug($loggedUser);
+        
     }
 }
--- app/MUser.php
+++ app/MUser.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MUser extends Model
 {
-    protected $table = 'my_flights';
+    protected $table = 'm_user';
     protected $primaryKey =  'user_id';
     public $timestamps = false;
 
-    protected $fillable = ['role_id','name','email','password','create_user_id','create_datetime','update_user_id','update_datetime','version'];
+    protected $fillable = ['name','email','password','create_user_id','create_datetime','update_user_id','update_datetime','version'];
 }
 
